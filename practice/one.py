ddcbea16e13be0d5b902a3653f46a7e02fe9b127
seouuun <syl77432@gmail.com>
2023-11-02 23:58:59 +0900

    day 2

--- practice/one.py
+++ practice/one.py
@@ -1,65 +1,95 @@
-# 숫자 자료형
-print(5)
-print(-10)
-print(3.14)
-print(1000)
-print(5+3)
-print(2*8)
-print(3*(3+1)) # 괄호 안에 괄호를 넣어 복잡한 사칙연산도 가능하다.
+'''# 연산자
+print(1 + 1) # 2
+print(3 - 1) # 1
+print(5 * 2) # 10
+print(6 / 3) # 2
+
+print(2 ** 3) # 2^3 = 8
+print(5 % 3) # = 2 나머지 구하기
+print(10 % 3) # = 1
+print(5 // 3) # = 1 몫 구하기
+print(10 // 3) # 3
+
+print(10 > 3) # True 앞의 값이 뒤의 값보다 크다.
+print(4 >=  7) # False 앞의 값이 뒤의 값보다 작거나 크다.
+print(10 < 3) # False 앞의 값이 뒤의 값보다 작다.
+print(5 <= 5) # True 앞의 값이 뒤의 값보다 작거나 같다.
+
+print(3 == 3) # True 앞의 값과 뒤의 값이 같다.
+print(4 == 2) # False 
+print(3 + 4 == 7) # True
+
+print(1 != 3) # True 앞의 값과 뒤의 값이 다르다.
+print(not(1 != 3)) # False
+
+print((3 > 0) and (3 < 5)) # True 앞의 항과 뒤의 항이 같아야 True이다.
+print((3 > 0) & (3 < 5)) # True 'and'를 쓰지 않고 '&'을 써도 된다.
+
+print((3 > 0) or (3 < 5)) # True 'or'을 이용하면 두 항 중 하나의 항만 같아도 True가 나온다.
+print((3 > 0) | (3 > 5)) # True 'or'을 쓰지 않고 '|'을 써도 된다. 
+
+print(5 > 4 > 3) # True 연속적인 부등식도 가능하다.
+print(5 > 4 > 7) # False
 
 
-# 문자열 자료형
-print('풍선')
-print("나비")
-print("ㅋㅋㅋㅋㅋㅋㅋㅋㅋ")
-print("ㅋ"*9) # 위의 경우처럼 'ㅋ' 을 9번 써도 되지만, *9를 사용해서 9개로 만들 수 있다.
+#간단한 수식
+print(2 + 3 * 4) # 14
+print((2 + 3) * 4) # 20
+number = 2 + 3 * 4 # 14
+print(number)
+number = number + 2 # 16 'number'변수에 2를 더한것이다.
+print(number)
+number += 2 # 18 이것도 마찬가지로 'number'변수에 2를 더한것이다.
+print(number)
+number *= 2 # 36 다른 부호를 사용할수도 있다.
+print(number)
+number /= 2 # 18
+print(number)
+number -= 2 # 16
+print(number)
+
+number %= 2 # 0 마찬가지로 나머지도 구할 수 있다.
 
 
-# 참 / 거짓
-print(5 > 10)
-print(5 <10)
-print(True)
-print(False)
-print(not True) # True 앞에 not을 붙이면 False, 즉 반대되는 결과가 나온다.
-print(not 5 > 10) # 5는 10보다 작기에 False가 나와야 하지만, 앞에 not을 붙여서 True가 나온다. 
+# 숫자 처리 함수
+print(abs(-5)) # 5 absolut, 즉 절대값이다.
+print(pow(4, 2)) # 4^2 = 4*4 = 16 앞의 수를 뒤의 수만큼 제곱한다.
+print(max(5, 12)) # 12 두 개의 숫자 중 더 큰 값을 출력한다.
+print(min(5, 12)) # 5 두 개의 숫자 중 더 작은 값을 출력한다.
+print(round(3.14)) # 3 반올림이다.
+print(round(4.99)) # 5 반올림이다.
+
+from math import * # 나중에 배울 내용이다. 'math'라이브러리 안에 있는 모든 것을 이용하겠다는 것이다.
+print(floor(4.99)) # 4 내림.
+print(ceil(3.14)) # 4 올림.
+print(sqrt(16)) # 4 제곱근.
 
 
-# 애완동물을 소개해 주세요~
-animal = "고양이" # 아래 코드를 바꿀 필요 없이 변수 부분만 바꾸면 되기에 편하다.
-name = "해피"
-age = 4
-hobby = "낮잠"
-is_adult = age >= 3 # 나이가 3 이상이면 어른이다.
-# 변수는 이름같은것을 하나하나 바꾸기 어려울떄 (코드가 길어질때) 한번에 바꾸기 위해 사용한다. 
-print("우리집 " + animal + " 이름은 "  + name + "이에요")
-hobby = "공놀이" # 위에서는 변수'hobby'가 낮잠으로 돼있지만, 코드 중간에서 변수'hobby'를 공놀이로 바꿨기에 변수'hobby'는 공놀이로 출력되었다.
-#print(name + "는 " + str(age) + "살이며, " + hobby + "을 아주 좋아해요") # 나이와 같은 정수형을 출력할 때에는 앞에 str을 붙이지 않으면 오류가 난다.
-print(name, "는 " + str(age), "살이며, " + hobby, "을 아주 좋아해요") # 위의 코드에서는 '+'를 사용하였지만 ','를 사용 할 수도 있다. 하지만 ','를 사용하면 문장 사이에 띄어쓰기가 들어가니 적절히 사용하자.
-print(name + "는 어른일까요? " + str(is_adult))
+#랜덤함수(난수)
+from random import *
+
+print(random()) # 0.0 ~ 1.0 미만의 임의의 값 생성.
+print(random() *  10) # 0.0 ~ 10.0 미만의 임의의 값 생성.
+print (int(random() * 10) + 1) # 1 ~ 10 이하의 임의의 값 생성
 
 
-'''이렇게
-작은따옴표를 3개 사용하면
-여러문장이
-주석처리된다'''
+print(int(random() * 45) + 1) # 1 ~ 45 이하의 임의의 값 생성.
 
-# 여러 문장을 주석 처리하고 싶을 때에는 ctrl + / 를 누르면 일괄 주석 처리 된다(해제도 마찬가지이다).
+print(randrange(1, 46)) # 1 ~ 46 미만의 임의의 값 생성.
+
+print(randint(1, 45)) # 1 ~ 45 이하의 임의의 값 생성.'''
 
 
-# Quiz) 변수를 이용해서 다음 문장을 출력하시오
+# Quiz) 당신은 최근에 코딩 스터디 모임을 새로 만들었습니다.
+# 월 4회 스터디를 하는데 3번은 온라인으로 하고 1번은 오프라인으로 하기로 했습니다.
+# 아래 조건에 맞는 오프라인 모임 날짜를 정해주는 프로그램을 작성하시오.
 
-# 변수명
-# :station
+# 조건1 : 랜덤으로 날짜를 뽑아야 함.
+# 조건2 : 월별 날짜는 다름을 감안하여 최소 일수인 28 이내로 정함.
+# 조건3 : 매월 1~3일은 스터디 준비를 해야 하므로 제외
 
-# 변수값
-# : "사당", "신도림", "인천공항", 순서대로 입력
-
-# 출력 문장
-# : XX행 열차가 들어오고 있습니다.
-
-station = "사당"
-print (station + "행 열차가 들어오고 있습니다.")
-station = "신도림"
-print (station + "행 열차가 들어오고 있습니다.")
-station = "인천공항"
-print (station + "행 열차가 들어오고 있습니다.")
+# (출력문 예제) 
+#모프라인 스터디 모임 날짜는 매월 x일로 선정되었습니다.
+from random import *
+date = randint(4, 28)
+print("오프라인 스터디 모임 날짜는 매월 " + str(date) + " 일로 선정되었습니다.")
